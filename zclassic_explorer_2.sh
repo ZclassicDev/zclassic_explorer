d204d48107b6480a252c105bd0558836004c13dd
J62 <j.owens@interbiznw.com>
2018-02-25 01:07:57 -0800

    Update zclassic_explorer_2.sh

--- zclassic_explorer_2.sh
+++ zclassic_explorer_2.sh
@@ -12,14 +12,14 @@ nvm install v4
 sudo apt-get -y install libzmq3-dev
 
 # install bitcore (branched and patched from https://github.com/z-classic/bitcore-node-zclassic.git)
-npm install z-classic/bitcore-node-zclassic
+npm install str4d/bitcore-node-zcash
 
 # create bitcore node
-./node_modules/bitcore-node-zclassic/bin/bitcore-node create zclassic-explorer
+./node_modules/bitcore-node-zcash/bin/bitcore-node create zclassic-explorer
 cd zclassic-explorer
 
 # install patched insight api/ui (branched and patched from https://github.com/z-classic/insight-api-zclassic & https://github.com/z-classic/insight-ui-zclassic)
-../node_modules/bitcore-node-zclassic/bin/bitcore-node install z-classic/insight-api-zclassic z-classic/insight-ui-zclassic
+../node_modules/bitcore-node-zcash/bin/bitcore-node install z-classic/insight-api-zclassic z-classic/insight-ui-zclassic
 
 # create bitcore config file for bitcore and zcashd/zclassicd
 # REPLACE "datadir" and "exec" with actual values of "/home/user"
@@ -36,26 +36,26 @@ cat << EOF > bitcore-node.json
   "servicesConfig": {
     "bitcoind": {
       "spawn": {
-        "datadir": "/home/user/.zclassic",  
-        "exec": "/home/user/zclassic/src/zcashd"     
+        "datadir": "/home/ubuntu/j62/.zclassic",
+        "exec": "/home/ubuntu/j62/zclassic/src/zcashd"
       }
     },
-     "insight-ui-zclassic": {
-      "apiPrefix": "api",
-      "routePrefix": ""
-
-     },
+    "insight-ui-zclassic": {
+      "apiPrefix": "api"
+    },
     "insight-api-zclassic": {
       "routePrefix": "api"
     }
   }
 }
 
+
 EOF
 
 # create zcash.conf
 cat << EOF > data/zclassic.conf
 server=1
+rpcport=8232
 whitelist=127.0.0.1
 txindex=1
 addressindex=1
@@ -69,7 +69,6 @@ rpcpassword=local321
 uacomment=bitcore
 showmetrics=1
 maxconnections=1000
-
 
 EOF
 
